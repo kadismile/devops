be0338796d6c06af6d0d6dd38dd79a0c30c64125
Ibrahim Abubakar <kadismile@MACs-MBP.lan>
2021-09-23 12:02:29 +0100

    aded final password reset

--- src/controllers/authController/recover_password.ts
+++ src/controllers/authController/recover_password.ts
@@ -5,6 +5,7 @@ import ApplicationError from '../../errors/application-error';
 import Joi from "@hapi/joi";
 import {Request, RequestHandler} from "express";
 import {sendEmail} from "../../helpers/send-email";
+import {compare, hash} from "bcryptjs";
 var kue = require('kue')
 
 export const RecoverPasswordSchema = Joi.object().keys({
@@ -46,6 +47,35 @@ const recoverPassword: RequestHandler = async (req: Request<{}, {}>, res) => {
   } else {
     res.status(401).json({
       message: "No user found With That Email"
+    });
+  }
+};
+
+const changePassword: RequestHandler = async (req: Request<{}, {}>, res) => {
+  let { oldPassword, newPassword, resetPasswordToken } = req.body;
+  const user: any =  await User.findOne({ resetPasswordToken: resetPasswordToken })
+  if (user) {
+    const verify = await compare(oldPassword, user.password);
+    if (!verify) {
+      res.status(403).json({
+        message: "Wrong Current Password Provided"
+      });
+    } else {
+      newPassword = await hash(newPassword, 13)
+      await User.findByIdAndUpdate(user._id, {
+       resetPasswordToken: "null", password: newPassword }, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+      });
+      res.status(200).json({
+        message: "Password Recovered Successfully"
+      });
+    }
+
+  } else {
+    res.status(401).json({
+      message: "No user found"
     });
   }
 };
