a01ae1c5f4c91748a1a886ffc9bc16c133c27d90
Ibrahim Abubakar <kadismile@MACs-MBP.lan>
2021-10-05 21:46:26 +0100

    added crud to category and specifications

--- src/controllers/categoryController/index.ts
+++ src/controllers/categoryController/index.ts
@@ -1,5 +1,12 @@
 import create_category from './create_category';
+import delete_category from './delete_category';
+import update_category from './update_category';
+import get_category from './get_category';
+
 
 export {
   create_category,
+  delete_category,
+  update_category,
+  get_category
 };
--- src/controllers/specificationController/index.ts
+++ src/controllers/specificationController/index.ts
@@ -1,7 +1,9 @@
 import create_specification from './create_specification';
 import update_specification from './update_specification';
+import delete_specification from './delete_specification';
 
 export {
   create_specification,
-  update_specification
+  update_specification,
+  delete_specification
 };
