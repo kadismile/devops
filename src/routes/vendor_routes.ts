c5b2c87227fa85d850283886c7507ca0d3a94c80
Ibrahim Abubakar <ikadismile@gmail.com>
2021-12-07 10:51:12 +0100

    added api for fetching vendors

--- src/routes/vendor_routes.ts
+++ src/routes/vendor_routes.ts
@@ -5,6 +5,7 @@ const vendor_routes = Router();
 
 // Vendor routes
 vendor_routes.post('/create', vendorController.create_vendor);
+vendor_routes.get('/', vendorController.get_vendors);
 
 
 export default vendor_routes;
