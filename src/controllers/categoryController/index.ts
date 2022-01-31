b9d11262748e51ffc95fdb4d30fb9877bb49322f
Ibrahim Abubakar <ikadismile@gmail.com>
2022-01-31 08:55:14 +0100

    make categories inactive

--- src/controllers/categoryController/index.ts
+++ src/controllers/categoryController/index.ts
@@ -1,5 +1,4 @@
 import create_category from './create_category';
-import delete_category from './delete_category';
 import update_category from './update_category';
 import get_category from './get_category';
 import { upload_category_by_csv } from './upload_category_by_csv';
@@ -7,7 +6,6 @@ import { upload_category_by_csv } from './upload_category_by_csv';
 
 export {
   create_category,
-  delete_category,
   update_category,
   get_category,
   upload_category_by_csv
