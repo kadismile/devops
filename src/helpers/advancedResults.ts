9d7fced442f6d618d17d99aa0da229f765676660
Ibrahim Abubakar <ikadismile@gmail.com>
2022-04-12 03:29:17 +0000

    added document count

--- src/helpers/advancedResults.ts
+++ src/helpers/advancedResults.ts
@@ -118,6 +118,7 @@ export const advancedResults = async (req: any, model: any, populate: any) => {
 
     let advancedResults = {
       count: results.length,
+      documentCount: total,
       pagination,
       data: results,
     };
